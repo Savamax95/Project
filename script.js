b18311dfe5b848bde8902ce502a7cb9874e5ff31
Savamax95 <60469454+Savamax95@users.noreply.github.com>
2020-02-11 13:41:35 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+/*
+let yourName = prompt("Как тебя зовут?");
+alert(`Привет, ${yourName},` );   Всплывающее окно
+*/
+// let yourName = prompt('Как тебя зовут?');
+// function myMessage() {
+// 	message = 'Привет, ' + yourName + '! Посмотри на мой сайт!';
+// 	alert(message);
+// }
+// myMessage();
+ 
+
---
+++ vue.js
@@ -0,0 +1,10 @@
+new Vue ({
+    el: '#app',
+    data: {
+    	show: false
+	},
+	methods: {
+		
+	}
+})
+
